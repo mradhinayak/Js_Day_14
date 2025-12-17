63e38c183d7a8dddcb8f059a7d8f48278af9a163
Shivam Goswami <mradhinayak@gmail.com>
2025-12-17 22:02:27 +0530

    Day 14 complete

--- solution.js
+++ solution.js
@@ -1,14 +1,44 @@
+// Insertion Sort in Ascending Order
 function insertionSortAsc(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
 
-        while (j >= 0 && arr[j] > current) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        arr[j + 1] = current;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
     }
-    return arr;
+    arr[j + 1] = current;
+  }
+  return arr;
 }
 console.log(insertionSortAsc([5, 2, 9, 1, 5, 6])); // [1, 2, 5, 5, 6, 9]
+
+// Insertion Sort in Descending Order
+function insertionSortDesc(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] < current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+  return arr;
+}
+console.log(insertionSortDesc([5, 2, 9, 1, 5, 6]));
+
+// Insert at correct position in Ascending Order;
+function insertionAtCorrectPlace(arr, value) {
+    arr.push(value);
+  let j = arr.length - 2;
+  while (j >= 0 && arr[j] > value) {
+    arr[j + 1] = arr[j];
+    j--;
+  }
+  arr[j + 1] = value;
+  return arr;
+}
+console.log(insertionAtCorrectPlace([1, 3, 5, 6], 4));
